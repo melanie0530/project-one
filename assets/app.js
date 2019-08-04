3177992838675db2ee78610be3d0ce3f744e225a
Melanie Mok <melaniemok530@gmail.com>
2019-08-04 13:03:34 -0700

    updated

--- assets/app.js
+++ assets/app.js
@@ -1,15 +1,15 @@
 var countries = {
     US:[
-      "Chicago"
+      "Chicago","Sacramento","Los Angeles","New York","Washington D.C","San Francisco","Seattle","Boston","Denver","Philadelphia","New Orleans","Nashville","Miami","Phoenix","Columbus","Indianapolis","Dallas","Bar Harbor","Wichita"
     ],
     UK:[
-      "London"
+      "London","Bristol","Liverpool","Edinburgh","Glasgow","Birmingham","Manchester","York","Cardiff","Belfast","Leeds","Newcastle"
     ],
     Australia:[
-      "Sydney"
+      "Sydney","Melbourne","Brisbase","Adelaide","Perth","Darwin","Hobart","Gold Coast","Cairns","Townville","Newcastle","Launceston"
     ],
     France:[
-      "Paris"
+      "Paris","Bordeaux","Nice","Marseille","Lyon","Toulouse","Lille"
     ]
 }
 
@@ -49,28 +49,49 @@ $("#search-weather").click(function () {
   $.ajax({
     url: "https://cors-anywhere.herokuapp.com/" + queryURL,
     method: "GET"
-  }).then(function (response) {
+  }).then(function (response) 
+  {
     var temp = Math.floor(response.main.temp);
     var minTemp = Math.floor(response.main.temp_min);
     var maxTemp = Math.floor(response.main.temp_max);
     var weather = response.weather[0].main;
+    var weatherDes = response.weather[0].description;
+    var iconcode = response.weather[0].icon;
+    var weatherIcon = "https://openweathermap.org/img/w/" + iconcode + ".png";
+    var city = response.name;
+    var country = response.sys.country;
 
     $(".weather").empty().append(weather);
-    $(".temp").empty().append(temp);
-    $(".minTemp").empty().append(minTemp);
-    $(".maxTemp").empty().append(maxTemp);
+    $(".weather-description").empty().append(weatherDes);
+    $(".temp").empty().append(temp,"°F");
+    $(".minTemp").empty().append(minTemp,"°F");
+    $(".maxTemp").empty().append(maxTemp,"°F");
+    $(".weather-icon").empty().attr('src',weatherIcon);
+    $(".weather-city").empty().append(city," ,");
+    $(".weather-country").empty().append(country);
 
+    $("#weather-in").empty().append("Weather In <br> ");
+    $("#current-temp").empty().append("Current Tempterature: ");
+    $("#min-temp").empty().append("Minimum Tempterature: ");
+    $("#max-temp").empty().append("Maximum Tempterature: ");
 
-  })
+  //   <button type="button" class = "weather"></button>
+
+  //   $(".weather").click(function () {
+  //     alert(weather-description);
+  //   })
+
+   })
 
 })
+
+
 
 // Exchange Currency 
 
 
 var queryURL = "http://data.fixer.io/api/latest?access_key=1fa6554da687fd32934b88c7e76bbfba&symbols=EUR,AUD,CAD,PLN,MXN,USD&format=1"
 
-//Exchange-button is from Melanie's HTML
 $("#exchange-button").on("click", function () {
 
   var initialAmount = $("#initial-dollar").val().trim();
@@ -105,52 +126,6 @@ $("#exchange-button").on("click", function () {
 
 });
 
-// $.getJSON(
-//   "api.openweathermap.org/data/2.5/weather?q="+ city +","+ country + "&units=imperial&appid=" + APIKey,
-
-//   function(data){
-//    console.log(data);
-
-//    var temp = Math.floor(data.main.temp);
-//    var weather=data.weather[0].main;
-
-//    $(".weather").append(weather);
-//    $(".temp").append("src",icon);
-
-//   }
-// )
-
-
-
-    // // Here we are building the URL we need to query the database
-    // var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-    //   "q="+ city +","+ country + "&units=imperial&appid=" + APIKey;
-
-    // // Here we run our AJAX call to the OpenWeatherMap API
-    // $.ajax({
-    //   url: queryURL,
-    //   method: "GET"
-    // })
-    //   // We store all of the retrieved data inside of an object called "response"
-    //   .then(function(response) {
-
-    //     // Log the queryURL
-    //     console.log(queryURL);
-
-    //     // Log the resulting object
-    //     console.log(response);
-
-    //     // Transfer content to HTML
-    //     $(".city").html("<h1>" + response.name + " Weather Details</h1>");
-    //     $(".wind").text("Wind Speed: " + response.wind.speed);
-    //     $(".humidity").text("Humidity: " + response.main.humidity);
-    //     $(".temp").text("Temperature (F) " + response.main.temp);
-
-    //     // Log the data in the console as well
-    //     console.log("Wind Speed: " + response.wind.speed);
-    //     console.log("Humidity: " + response.main.humidity);
-    //     console.log("Temperature (F): " + response.main.temp);
-    //   });
 
 
     
